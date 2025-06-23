53d34b5a013a1af0d4c1cbbc99b8461b11bedc9e
MichelleLeonSinger <michelle.leon.singer@gmail.com>
2025-06-22 18:05:00 -0600

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  base: '/LANDING_PAGE_MICHELLE_LEON/', // cambia esto según tu repositorio
+  plugins: [react()],
+});
