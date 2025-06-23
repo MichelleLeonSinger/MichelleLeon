4c56b208c8c62478dc7bb5188e27f6652a93624c
MichelleLeonSinger <michelle.leon.singer@gmail.com>
2025-06-22 18:12:56 -0600

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: '/LANDING_PAGE_MICHELLE_LEON/', // cambia esto según tu repositorio
-  plugins: [react()],
-});
